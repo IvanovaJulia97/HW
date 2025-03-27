d9d03a9efd8cb54550133f1d5abbfbb0730e18c9
IvanovaJulia97 <ivanova.julia.1997@mail.ru>
2025-03-27 22:40:32 +0300

    HomeServer

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	localServer := server.MyServer(logger)
+
+	if err := localServer.Start(); err != nil {
+		logger.Fatalf("Ошибка запуска сервера: %v", err)
+	}
 }
